e5b42549208a8d931ea60778fe89f70c58fc937f
Christopher Leja <christopher.leja@gmail.com>
2020-02-19 22:52:19 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,9 +5,9 @@ class School
       @roster = {}
     end
   
-    def add_student(name, grade)
-      @roster["#{grade}"] = []
-      @roster["#{grade}"] << name
+    def add_student(student, grade)
+      @roster[grade] = []
+      @roster[grade] << student
     end
     
     def roster
